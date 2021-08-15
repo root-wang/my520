1a7b584d44983a5e5ee6b78be4a7da2c354e4cae
w276211640 <85927003+w276211640@users.noreply.github.com>
2021-08-16 01:57:55 +0800

    update README.md

--- read-note/src/store.js
+++
@@ -1,7 +0,0 @@
-import {writable} from "svelte/store";
-import Cookies from "js-cookie";
-
-// 上次的卡片列表
-let lastCards = JSON.parse(Cookies.get('cards') || '[]') ;
-
-export const cards = writable(lastCards);
--- yulang/core/doc.js
+++
@@ -1,5 +0,0 @@
-const {wordEmojiMap} = require('./map');
-console.log('📚 Document is shown below: ')
-for (let word in wordEmojiMap) {
-    console.log(`${word} => ${wordEmojiMap[word]}`);
-}
